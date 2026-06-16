5746d309410307a0b04495c1de5152d3cefee51b
natanaeladitya <natanaela@cynapse.ai>
2026-06-16 14:12:58 +0800

    add operations

--- Program.cs
+++ Program.cs
@@ -7,4 +7,14 @@ const int b = 12;
 var c = Interop.add(a, b);
 Console.WriteLine($"{a} + {b} = {c}");
 
+const int d = 22;
+const int e = 33;
+var f = Interop.add(d, e);
+Console.WriteLine($"{d} + {e} = {f}");
+
+const int g = 44;
+const int h = 55;
+var i = Interop.add(g, h);
+Console.WriteLine($"{g} + {h} = {i}");
+
 Console.WriteLine("Done.");
